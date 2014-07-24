e4c4bd465d3b80425b15bd31502f0ce8d386d1af
Adland Lee <adland[at]4mfd[dot]com>
2014-07-24 12:47:54 -0700

    Initial Commit

---
+++ Spectrum Analyser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spectrum Analyser
+//
+//  Created by Adland Lee on 7/24/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Spectrum AnalyserTests/Spectrum_AnalyserTests.m
@@ -0,0 +1,40 @@
+//
+//  Spectrum_AnalyserTests.m
+//  Spectrum AnalyserTests
+//
+//  Created by Adland Lee on 7/24/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Spectrum_AnalyserTests : XCTestCase
+
+@end
+
+@implementation Spectrum_AnalyserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
