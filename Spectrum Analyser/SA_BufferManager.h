5afedd15012ae09a14903265c65d5433225970c0
Adland Lee <adland[at]4mfd[dot]com>
2014-07-28 16:19:53 -0700

    Update BufferManager Interface

--- Spectrum Analyser/SA_BufferManager.h
+++ Spectrum Analyser/SA_BufferManager.h
@@ -12,7 +12,48 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <libkern/OSAtomic.h>
 
+#include "SA_FFTHelper.h"
+
 const UInt32 kNumDrawBuffers = 12;
 const UInt32 kDefaultDrawSamples = 1024;
+
+class BufferManager
+{
+private:
+    UInt32 _DisplayMode;
+    
+    Float32* _drawBuffers[kNumDrawBuffers];
+    UInt32 _drawBuffersIndex;
+    UInt32 _currentDrawBufferLength;
+    
+    Float32* _FFTInputBuffer;
+    UInt32 _FFTInputBufferFrameIndex;
+    UInt32 _FFTInputBufferLength;
+    volatile int32_t _hasNewFFTData;
+    volatile int32_t _needsNewFFTData;
+    
+    FFTHelper*  _FFTHelper;
+
+public:
+    BufferManager (UInt32 inMaxFramesPerSlice);
+    ~BufferManager ();
+    
+    void SetDisplayMode (UInt32 inDisplayMode) { _DisplayMode = inDisplayMode; }
+    UInt32 GetDisplayMode () { return _DisplayMode; }
+
+    Float32** GetDrawBuffers () { return _drawBuffers; }
+    void CopyAudioDataToDrawBuffer (Float32* inData, UInt32 numberOfFrames);
+    void CycleDramBuffers ();
+
+    void SetCurrentDrawBufferLength (UInt32 inDrawBufferLength) { _currentDrawBufferLength = inDrawBufferLength; }
+    UInt32 GetCurrentDrawBufferLength () { return _currentDrawBufferLength; }
+    
+    bool HasNewFFTData () { return static_cast<bool>(_hasNewFFTData); }
+    bool NeedsNewFFTData () { return static_cast<bool>(_needsNewFFTData); }
+    
+    void CopyAduioDataToFFTInputBuffer (Float32* inData, UInt32 numberOfFrames);
+    UInt32 GetFFTOutputBufferLength () { return _FFTInputBufferLength / 2; }
+    void GetFFTOutput (Float32* outFFTData);
+};
 
 #endif /* defined(__Spectrum_Analyser__SA_BufferManager__) */
