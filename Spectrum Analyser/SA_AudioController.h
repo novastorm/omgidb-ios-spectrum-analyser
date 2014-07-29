1f89d9b1f458f6ff8f548d2934d42fc26e7a30c3
Adland Lee <adland[at]4mfd[dot]com>
2014-07-28 18:14:46 -0700

    Add AudioController interface

--- Spectrum Analyser/SA_AudioController.h
+++ Spectrum Analyser/SA_AudioController.h
@@ -6,8 +6,27 @@
 //  Copyright (c) 2014 Adland Lee. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+
+#import "SA_BufferManager.h"
+#import "SA_DCRejectionFilter.h"
 
 @interface SA_AudioController : NSObject
+{
+    AudioUnit _rioUnit;
+    BufferManager* _bufferManager;
+    DCRejectionFilter* _DCRejectionFilter;
+    AVAudioPlayer* _audioPlayer;
+}
+
+@property (nonatomic, assign) BOOL muteAudio;
+@property (nonatomic, assign, readonly) BOOL audioChainIsBeingReconstructed;
+
+- (BufferManager*) getBufferManagerInstance;
+- (OSStatus) startIOUnit;
+- (OSStatus) stopIOUnit;
+- (void) playButtonPressedSound;
+- (double) sessionSampleRate;
 
 @end
