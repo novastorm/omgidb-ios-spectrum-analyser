8aa5b3528ea325b7487c962cdf3534e50ba80c43
Adland Lee <adland[at]4mfd[dot]com>
2014-08-01 15:43:50 -0700

    remove unused method

--- Spectrum Analyser/AppDelegate.m
+++ Spectrum Analyser/AppDelegate.m
@@ -15,19 +15,6 @@
 @implementation AppDelegate
             
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
-//    CGRect screenBounds = [[UIScreen mainScreen] bounds];
-//    DLog(@"allocate _view");
-//    self.view = [[SA_EAGLView alloc] initWithFrame:screenBounds];
-//    DLog(@"add subview");
-//    [self.window addSubview:_view];
-//    DLog(@"done");
-    
-    return YES;
-}
-
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
