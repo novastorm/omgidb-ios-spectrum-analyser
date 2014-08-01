724a234d9955928624c4a0e0bcf04962f727ffc2
Adland Lee <adland[at]4mfd[dot]com>
2014-08-01 15:46:28 -0700

    project updates

---
+++ Spectrum Analyser/SA_ViewController.h
@@ -0,0 +1,19 @@
+//
+//  SA_ViewController.h
+//  Spectrum Analyser
+//
+//  Created by Adland Lee on 7/24/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@class AGLKVertexAttribArrayBuffer;
+
+@interface SA_ViewController : GLKViewController
+
+@property (nonatomic) GLKBaseEffect *baseEffect;
+@property (nonatomic) AGLKVertexAttribArrayBuffer *vertexBuffer;
+
+@end
+
